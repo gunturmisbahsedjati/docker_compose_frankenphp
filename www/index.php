4080745c567ea343533b784143605acdc05d9be5
gunturmisbahsedjati <gunturmisbahsedjati@gmail.com>
2025-06-05 10:55:28 +0700

    upload docker compose

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server Docker Ku 😁</title>
+</head>
+
+<body>
+    <h1 align="center">Hi There ! 👋</h1>
+    <?php echo '<img src="https://frankenphp.dev/img/elephant_footer.svg" alt="" width="100" height="100" /><br>Powered by <a href="https://frankenphp.dev">FrankenPHP</a>'; ?>
+</body>
+
+</html>
