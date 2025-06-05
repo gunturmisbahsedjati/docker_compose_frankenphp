36466bb3f09724aff553a211ec01b66a9dd60bf5
gunturmisbahsedjati <gunturmisbahsedjati@gmail.com>
2025-06-05 11:28:46 +0700

    ubah index.php2

--- www/index.php
+++ www/index.php
@@ -9,6 +9,7 @@
 
 <body align="center">
     <h1>Hi There ! 👋</h1>
+    <h5>FrankenPHP sudah berjalan....<br>Silahkan update path laravel !</h5>
     <img src="https://frankenphp.dev/img/elephant_footer.svg" alt="" width="100" height="100" /><br>Powered by <a href="https://frankenphp.dev">FrankenPHP</a>
 </body>
 
